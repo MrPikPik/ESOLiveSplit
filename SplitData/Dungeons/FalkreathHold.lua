44765755476dd421db511a73d6b493dbb2d86194
MrPikPik <mrpikpikk@googlemail.com>
2023-10-22 21:56:58 +0200

    Fix: FH splits

--- SplitData/Dungeons/FalkreathHold.lua
+++ SplitData/Dungeons/FalkreathHold.lua
@@ -1,41 +1,33 @@
 local splits = {
 	[1] = {
-		name = "Trash 1",
-		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
-	},
-	[2] = {
-		name = "Morrigh Bullblood",
+		name = "Morrigh Bullblood & Trash",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
 	},
-	[3] = {
-		name = "Trash 2",
-		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
-	},
-	[4] = {
+	[2] = {
 		name = "The Siege Mammoth",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
 	},
-	[5] = {
+	[3] = {
 		name = "Trash 3",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
 	},
-	[6] = {
+	[4] = {
 		name = "Cernunnon",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
 	},
-	[7] = {
+	[5] = {
 		name = "Trash 4",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
 	},
-	[8] = {
+	[6] = {
 		name = "Deathlord Bjarfrud Skjoralmor",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
 	},
-	[9] = {
+	[7] = {
 		name = "Trash 5",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
 	},
-	[10] = {
+	[8] = {
 		name = "Domihaus",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
 	},
