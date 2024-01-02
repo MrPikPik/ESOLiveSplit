1868540ba4c2bba4f3fc8004d68a0477c2b62557
MrPikPik <mrpikpikk@googlemail.com>
2024-01-02 02:46:39 +0100

    CoH1: Added Skip/NoSkip variants

--- SplitData/Dungeons/CryptOfHearts1.lua
+++ SplitData/Dungeons/CryptOfHearts1.lua
@@ -1,4 +1,4 @@
-local splits = {
+local splitsNoSkip = {
 	[1] = {
 		name = "Trash 1",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
@@ -40,28 +40,80 @@ local splits = {
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
 	},
 	[11] = {
-		name = "Trash 6",
-		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
-	},
-	[12] = {
 		name = "The Ilambris Twins",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
 	},
 }
 
+local splitsSkip = {
+	[1] = {
+		name = "Trash 1",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[2] = {
+		name = "The Mage Master",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[3] = {
+		name = "Trash 2",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[4] = {
+		name = "Archmaster Siniel",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[5] = {
+		name = "Trash 3",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[6] = {
+		name = "Death's Leviathan",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[7] = {
+		name = "Trash 4",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[8] = {
+		name = "Dogas The Berserker",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[9] = {
+		name = "The Ilambris Twins",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+}
+
+
 SPLIT_MANAGER:RegisterSplit(130, DUNGEON_DIFFICULTY_NORMAL, {
-	catName = "Crypt of Hearts I",
-	menuName = "Any%",
+	catName = "Crypt of Hearts I - Skip",
+	menuName = "Skip",
 	startTrigger = LIVE_SPLIT_TRIGGER_ENTER_COMBAT,
 	wr = 1200000,
 	wrPlayer = "Zenimax",
-	splits = splits
+	splits = splitsSkip
 })
 SPLIT_MANAGER:RegisterSplit(130, DUNGEON_DIFFICULTY_VETERAN, {
-	catName = "Crypt of Hearts I",
-	menuName = "Any%",
+	catName = "Crypt of Hearts I - Skip",
+	menuName = "Skip",
 	startTrigger = LIVE_SPLIT_TRIGGER_ENTER_COMBAT,
 	wr = 1200000,
 	wrPlayer = "Zenimax",
-	splits = splits
+	splits = splitsSkip
+})
+SPLIT_MANAGER:RegisterSplit(130, DUNGEON_DIFFICULTY_NORMAL, {
+	catName = "Crypt of Hearts I - NoSkip",
+	menuName = "NoSkip",
+	startTrigger = LIVE_SPLIT_TRIGGER_ENTER_COMBAT,
+	wr = 1200000,
+	wrPlayer = "Zenimax",
+	splits = splitsNoSkip
+})
+SPLIT_MANAGER:RegisterSplit(130, DUNGEON_DIFFICULTY_VETERAN, {
+	catName = "Crypt of Hearts I - NoSkip",
+	menuName = "NoSkip",
+	startTrigger = LIVE_SPLIT_TRIGGER_ENTER_COMBAT,
+	wr = 1200000,
+	wrPlayer = "Zenimax",
+	splits = splitsNoSkip
 })
