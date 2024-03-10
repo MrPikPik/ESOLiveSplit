962795667c0f2ae2cd04b6e5279be6a9bbc1e806
MrPikPik <mrpikpikk@googlemail.com>
2024-03-10 15:13:00 +0100

    Added "Scions of Ithelia" dungeons

---
+++ SplitData/Dungeons/BedlamVeil.lua
@@ -0,0 +1,47 @@
+local splits = {
+	[1] = {
+		name = "Trash 1",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[2] = {
+		name = "Shattered Champion",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[3] = {
+		name = "Trash 2",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[4] = {
+		name = "Darkshard",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[5] = {
+		name = "Trash 3",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[6] = {
+		name = "The Blind",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+}
+
+SPLIT_MANAGER:RegisterSplit(1471, DUNGEON_DIFFICULTY_NORMAL, {
+	id = "ESOLS_BedlamVeil",
+	catName = "Bedlam Veil",
+	menuName = "Official",
+	startTrigger = LIVE_SPLIT_TRIGGER_ENTER_COMBAT,
+	par = LIVE_SPLIT_TIME_25_MINUTES,
+	wr = 1500000,
+	wrPlayer = "Zenimax",
+	splits = splits
+})
+SPLIT_MANAGER:RegisterSplit(1471, DUNGEON_DIFFICULTY_VETERAN, {
+	id = "ESOLS_BedlamVeil",
+	catName = "Bedlam Veil",
+	menuName = "Official",
+	startTrigger = LIVE_SPLIT_TRIGGER_ENTER_COMBAT,
+	par = LIVE_SPLIT_TIME_25_MINUTES,
+	wr = 1500000,
+	wrPlayer = "Zenimax",
+	splits = splits
+})
---
+++ SplitData/Dungeons/OathswornPit.lua
@@ -0,0 +1,47 @@
+local splits = {
+	[1] = {
+		name = "Trash 1",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[2] = {
+		name = "Packmaster Rethelros",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[3] = {
+		name = "Trash 2",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[4] = {
+		name = "Anthelmir & Construct",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[5] = {
+		name = "Trash 3",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[6] = {
+		name = "Aradros The Awakened",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+}
+
+SPLIT_MANAGER:RegisterSplit(1470, DUNGEON_DIFFICULTY_NORMAL, {
+	id = "ESOLS_OathswornPit",
+	catName = "Oathsworn Pit",
+	menuName = "Official",
+	startTrigger = LIVE_SPLIT_TRIGGER_ENTER_COMBAT,
+	par = LIVE_SPLIT_TIME_25_MINUTES,
+	wr = 1500000,
+	wrPlayer = "Zenimax",
+	splits = splits
+})
+SPLIT_MANAGER:RegisterSplit(1470, DUNGEON_DIFFICULTY_VETERAN, {
+	id = "ESOLS_OathswornPit",
+	catName = "Oathsworn Pit",
+	menuName = "Official",
+	startTrigger = LIVE_SPLIT_TRIGGER_ENTER_COMBAT,
+	par = LIVE_SPLIT_TIME_25_MINUTES,
+	wr = 1500000,
+	wrPlayer = "Zenimax",
+	splits = splits
+})
