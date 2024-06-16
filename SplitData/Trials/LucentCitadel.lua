eaa474a7bbb90465cb758e5315eaca9489c08a5b
MrPikPik <mrpikpikk@googlemail.com>
2024-06-16 22:55:08 +0200

    Added 'Lucent Citadel'

---
+++ SplitData/Trials/LucentCitadel.lua
@@ -0,0 +1,113 @@
+local startData = {
+	x = 138450,
+	y = 34977,
+	z = 168744,
+	r = 4.0,
+}
+
+local splitsNormal = {
+	[1] = {
+		name = "Trash 1",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[2] = {
+		name = "Count Ryelaz & Zilyesset",
+		icon = LIVE_SPLIT_ICON_BOSS,
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[3] = {
+		name = "Trash 2",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[4] = {
+		name = "Cavot Agnan",
+		icon = LIVE_SPLIT_ICON_BOSS,
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[5] = {
+		name = "Trash 3",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[6] = {
+		name = "Orphic Shattered Shard",
+		icon = LIVE_SPLIT_ICON_BOSS,
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[7] = {
+		name = "The Arcane Knot",
+		icon = LIVE_SPLIT_ICON_ENDBOSS,
+		splitTrigger = LIVE_SPLIT_TRIGGER_NPC_MESSAGE,
+		data = {
+			match = {
+				["en"] = "The energy is contained!",
+			},
+		},
+	},
+}
+
+local splitsVeteran = {
+	[1] = {
+		name = "Trash 1",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[2] = {
+		name = "Count Ryelaz & Zilyesset",
+		icon = LIVE_SPLIT_ICON_BOSS,
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[3] = {
+		name = "Trash 2",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[4] = {
+		name = "Cavot Agnan",
+		icon = LIVE_SPLIT_ICON_BOSS,
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[5] = {
+		name = "Trash 3",
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+	},
+	[6] = {
+		name = "Orphic Shattered Shard",
+		icon = LIVE_SPLIT_ICON_BOSS,
+		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_DEATH,
+	},
+	[7] = {
+		name = "The Arcane Knot",
+		icon = LIVE_SPLIT_ICON_ENDBOSS,
+		splitTrigger = LIVE_SPLIT_TRIGGER_END_TRIAL,
+	},
+}
+
+SPLIT_MANAGER:RegisterSplit(1478, DUNGEON_DIFFICULTY_NORMAL, {
+	id = "ESOLS_LucentCitadel",
+	catName = "nLC",
+	menuName = "Official",
+	startTrigger = LIVE_SPLIT_TRIGGER_LOCATION,
+	startData = startData,
+	par = LIVE_SPLIT_TIME_30_MINUTES,
+	wr = 1800000,
+	wrPlayer = "Zenimax",
+	splits = splitsNormal,
+})
+SPLIT_MANAGER:RegisterSplit(1478, DUNGEON_DIFFICULTY_VETERAN, {
+	id = "ESOLS_LucentCitadel",
+	catName = "vLC",
+	menuName = "Official",
+	startTrigger = LIVE_SPLIT_TRIGGER_BEGIN_TRIAL,
+	par = LIVE_SPLIT_TIME_30_MINUTES,
+	wr = 1800000,
+	wrPlayer = "Zenimax",
+	splits = splitsVeteran,
+})
+SPLIT_MANAGER:RegisterSplit(1478, DUNGEON_DIFFICULTY_VETERAN, {
+	id = "ESOLS_LucentCitadel_HM",
+	catName = "vLC HM",
+	menuName = "Official HM",
+	startTrigger = LIVE_SPLIT_TRIGGER_BEGIN_TRIAL,
+	par = LIVE_SPLIT_TIME_30_MINUTES,
+	wr = 1800000,
+	wrPlayer = "Zenimax",
+	splits = splitsVeteran,
+})
