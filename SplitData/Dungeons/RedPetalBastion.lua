a7fee1a9d9ad96484e1da19dabfa7bd6ef01d3bc
MrPikPik <mrpikpikk@googlemail.com>
2024-12-01 23:22:34 +0100

    Fix: RPB sideboss filters

--- SplitData/Dungeons/RedPetalBastion.lua
+++ SplitData/Dungeons/RedPetalBastion.lua
@@ -2,6 +2,11 @@ local splits = {
 	[1] = {
 		name = "Trash 1",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+		data = {
+			filterMatch = {
+				[1] = "Rogerain",
+			},
+		},
 	},
 	[2] = {
 		name = "Rogerain The Sly",
@@ -11,6 +16,11 @@ local splits = {
 	[3] = {
 		name = "Trash 2",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+		data = {
+			filterMatch = {
+				[1] = "Merick",
+			},
+		},
 	},
 	[4] = {
 		name = "The Artifact Bearers",
@@ -20,6 +30,11 @@ local splits = {
 	[5] = {
 		name = "Trash 3",
 		splitTrigger = LIVE_SPLIT_TRIGGER_BOSS_ENTER,
+		data = {
+			filterMatch = {
+				[1] = "Thierric",
+			},
+		},
 	},
 	[6] = {
 		name = "Prior Thierric Sarazen",
